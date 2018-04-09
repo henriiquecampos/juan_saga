f232b72913ad154eebf8fd51ebd53309748c8d4a
Henrique Campos <henriiquecampos@gmail.com>
2018-04-09 11:17:42 -0300

    Added dedictated script to level scenes
    Also added 2 new levels

---
+++ levels/level_00.gd
@@ -0,0 +1,17 @@
+extends Node2D
+export (String, FILE, "*.tscn") var next_level
+
+func _ready():
+	score_container.next_level = next_level
+	$rocket.connect("tree_exited", self, "_on_rocket_tree_exited")
+	
+func _on_timer_timeout():
+	get_tree().set_pause(true)
+	$animator.play("release")
+	yield($animator, "animation_finished")
+	$juan/camera.tween_position()
+	yield($juan/camera/tween, "tween_completed")
+	$rocket.launch()
+
+func _on_rocket_tree_exited():
+	$screen/game_screen.change_scene()
