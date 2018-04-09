162e139a43a7b739b2ea072172b8f80bfd83f3f7
Henrique Campos <henriiquecampos@gmail.com>
2018-04-09 11:18:38 -0300

    Changed Bugs behaviors to expose data

--- actors/enemies/bugs.gd
+++ actors/enemies/bugs.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 onready var score_node = get_tree().get_nodes_in_group("score")[0]
 
-func _on_screen_bug_tree_exited():
+func _on_screen_bug_tree_exited(bug):
 	$sfx.play()
-	score_node.set_score(15, score_node.ADD)
+	score_node.set_score(bug.score, score_node.ADD)
 	
-func _on_screen_bug_timeout():
-	score_node.set_score(5, score_node.REMOVE)
+func _on_screen_bug_timeout(bug):
+	score_node.set_score(bug.damage, score_node.REMOVE)
--- actors/enemies/ground_bug.gd
+++ actors/enemies/ground_bug.gd
@@ -1,10 +1,12 @@
 extends "res://actors/platform_actors/platform_character.gd"
 
-var health = 3 setget set_health
+export(int) var health = 3 setget set_health
+export(int) var bugs_spawn = 1
+export(int) var score = 15
 const SCREEN_BUG = preload("res://actors/enemies/screen_bug.tscn")
 
 func _ready():
-	$button.connect("button_up", self, "set_health", [-1])
+	$button.connect("button_up", self, "damage_health", [1])
 	
 func _on_area_entered(area):
 	if area.is_in_group("player"):
@@ -14,16 +16,19 @@ func _on_area_entered(area):
 
 func _on_attach_area_entered(area):
 	if area.is_in_group("player"):
-		var s = SCREEN_BUG.instance()
-		s.position.x = rand_range(-16, 16)
-		s.position.y = rand_range(-16, 16)
-		area.get_node("../bugs").add_child(s)
+		for i in bugs_spawn:
+			var s = SCREEN_BUG.instance()
+			s.position.x = rand_range(-16, 16)
+			s.position.y = rand_range(-16, 16)
+			area.get_node("../bugs").add_child(s)
 		queue_free()
 		
 func set_health(value):
-	if health > 1:
-		health += value
-	else:
-		get_parent()._on_screen_bug_tree_exited()
+	health = value
+	if health < 1:
+		get_parent()._on_screen_bug_tree_exited(self)
 		get_parent().get_node("sfx").position = position
 		queue_free()
+
+func damage_health(amount):
+	set_health(health - amount)
