858b02a6df4242a02859ddd12c227c8fd36fd452
Henrique Campos <henriiquecampos@gmail.com>
2018-04-09 11:16:28 -0300

    Fixed SFX playing when mainscreen is ready

--- screens/main_menu/main_screen.gd
+++ screens/main_menu/main_screen.gd
@@ -1,5 +1,5 @@
 extends "res://screens/basic_screen.gd"
-
+var can_play = false
 func _ready():
 	var bgm = AudioServer.get_bus_index("bgm")
 	$bgm_volume.value = AudioServer.get_bus_volume_db(bgm)
@@ -11,6 +11,10 @@ func _on_bgm_volume_value_changed(value):
 	AudioServer.set_bus_volume_db(bgm, value)
 
 func _on_sfx_volume_value_changed(value):
-	$sfx_volume/sfx_test.play()
 	var sfx = AudioServer.get_bus_index("sfx")
 	AudioServer.set_bus_volume_db(sfx, value)
+	if !can_play:
+		can_play = true
+		return
+	$sfx_volume/sfx_test.play()
+	
