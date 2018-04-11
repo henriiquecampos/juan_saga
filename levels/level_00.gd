a4e2e4b3f62439bddfa3cc3025f70834f4273cb2
Henrique Campos <henriiquecampos@gmail.com>
2018-04-11 15:38:56 -0300

    Added check for backer players

--- levels/level_00.gd
+++ levels/level_00.gd
@@ -4,6 +4,9 @@ export (String, FILE, "*.tscn") var next_level
 func _ready():
 	score_container.next_level = next_level
 	$rocket.connect("tree_exited", self, "_on_rocket_tree_exited")
+	yield($screen/game_screen/animator, "animation_finished")
+	if !score_container.already_played:
+		$tutoring/animator.play("tutoring_00")
 	
 func _on_timer_timeout():
 	get_tree().set_pause(true)
