764af869a76617b838edf256f4173025fc0e9d27
Henrique Campos <henriiquecampos@gmail.com>
2018-04-12 15:09:15 -0300

    Fixed screen bugs dying with 1 click

--- actors/enemies/screen_bug.gd
+++ actors/enemies/screen_bug.gd
@@ -1,9 +1,9 @@
 extends Node2D
-var health = 3 setget set_health
+export (int) var health = 3 setget set_health
 export (int) var damage = 5
 export (int) var score = 15
 func _ready():
-	$button.connect("button_up", self, "set_health", [-1])
+	$button.connect("button_up", self, "damage_health", [1])
 	$timer.set_wait_time(rand_range(0.5, 2))
 	connect("tree_exited", get_parent(), "_on_screen_bug_tree_exited", [self])
 	$timer.connect("timeout", get_parent(), "_on_screen_bug_timeout", [self])
