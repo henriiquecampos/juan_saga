e46496caf51ffe5dc50ca8b0085f43606e66f3fd
Henrique Campos <henriiquecampos@gmail.com>
2018-04-12 15:11:28 -0300

    Added Retry option passing current scene to score_container

--- screens/game_over/game_over_screen.gd
+++ screens/game_over/game_over_screen.gd
@@ -2,3 +2,7 @@ extends "res://screens/basic_screen.gd"
 
 func _ready():
 	next_scene = score_container.next_level
+
+func _on_retry_button_up():
+	next_scene = score_container.current_level
+	change_scene()
