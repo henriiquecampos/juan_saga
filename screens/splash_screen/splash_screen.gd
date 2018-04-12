6d9e6735c87249cbbc838a3010729ac7e48879e8
Henrique Campos <henriiquecampos@gmail.com>
2018-04-12 09:16:40 -0300

    Added JimJam splash screen

--- screens/splash_screen/splash_screen.gd
+++ screens/splash_screen/splash_screen.gd
@@ -1,8 +1,8 @@
 extends "res://screens/basic_screen.gd"
 
-
+export (String) var new_anim
 func _on_animator_animation_finished(anim_name):
 	if anim_name == "fade":
-		$animator.play("pigdev_splash")
+		$animator.play(new_anim)
 		yield($animator, "animation_finished")
 		change_scene()
