555e687c206b712d19caa9908b6e189a81a2b12e
Henrique Campos <henriiquecampos@gmail.com>
2018-04-13 09:53:12 -0300

    Added uncompleted level interface and method

--- interface/hud/hud.gd
+++ interface/hud/hud.gd
@@ -5,3 +5,11 @@ onready var level = get_node(level_path)
 
 func _ready():
 	$counter/timer.connect("timeout", level, "_on_timer_timeout")
+	
+func uncomplete():
+	for c in get_children():
+		c.hide()
+	$uncompleted.show()
+
+func _on_retry_button_up():
+	get_tree().reload_current_scene()
