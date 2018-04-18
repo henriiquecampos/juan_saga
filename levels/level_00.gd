9985152ac5bfb818425ea8f8d31550b6d1d8575c
Henrique Campos <henriiquecampos@gmail.com>
2018-04-18 12:12:38 -0300

    Changed background music to levels

--- levels/level_00.gd
+++ levels/level_00.gd
@@ -2,8 +2,13 @@ extends Node2D
 export (String, FILE, "*.tscn") var next_level
 export (String, FILE, "*.tscn") var current_level
 export (int) var min_score = 0
+const BGM_MUSIC = preload("res://assets/bgm/background_music.ogg")
 
 func _ready():
+	if !bgm.stream == BGM_MUSIC:
+		bgm.stream = BGM_MUSIC
+		bgm.get_node("animator").play("fade")
+		bgm.play()
 	$rocket.set_minimum_score(min_score)
 	score_container.next_level = next_level
 	score_container.current_level = current_level
