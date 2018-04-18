f033574043712752eefcd228d4acc52ca5af26b5
Henrique Campos <henriiquecampos@gmail.com>
2018-04-18 12:11:57 -0300

    Added main menu music to main_screen

--- screens/main_menu/main_screen.gd
+++ screens/main_menu/main_screen.gd
@@ -1,10 +1,13 @@
 extends "res://screens/basic_screen.gd"
 var can_play = false
+const BGM_MUSIC = preload("res://assets/bgm/main_menu_music.ogg")
 func _ready():
 	var bus = AudioServer.get_bus_index("bgm")
 	$bgm_volume.value = AudioServer.get_bus_volume_db(bus)
 	bus = AudioServer.get_bus_index("sfx")
 	$sfx_volume.value = AudioServer.get_bus_volume_db(bus)
+	if !bgm.stream == BGM_MUSIC:
+		bgm.stream = BGM_MUSIC
 	if !bgm.is_playing():
 		bgm.play()
 	
