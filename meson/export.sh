56539af628722e26367e6e5e6f589433233e5bf0
Cassidy James Blaede <c@ssidyjam.es>
2018-05-16 23:35:43 -0600

    Install the binary and correct .desktop exec line

--- meson/export.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Export with godot and the Linux prefix defined in the project
-godot --export Linux ${DESTDIR}/${MESON_BUILD_ROOT}/com.github.pigdevstudio.juan_saga
-
-# Fix godot .pck file name
-mv ${DESTDIR}/${MESON_BUILD_ROOT}/com.github.pigdevstudio.pck ${DESTDIR}/${MESON_BUILD_ROOT}/com.github.pigdevstudio.juan_saga.pck
-
